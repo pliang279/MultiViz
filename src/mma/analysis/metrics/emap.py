214dcc00fa73b34cec58350dea2cb42b6d430735
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-08-29 13:37:48 +0530

    Allow Dict and Tuple inputs in EMAP

--- src/mma/analysis/metrics/emap.py
+++ src/mma/analysis/metrics/emap.py
@@ -6,7 +6,7 @@ the original model scores indicate a more cross-modal model.
 """
 
 from itertools import product
-from typing import Callable, Union, Dict
+from typing import Callable, Union, Dict, Tuple
 
 import numpy as np
 
@@ -15,18 +15,47 @@ class Emap:
     def __init__(
         self,
         predictor_fn: Callable,
-        dataset: Dict[str, np.ndarray],
+        dataset: Dict[str, Union[Dict[str, np.ndarray], Tuple[np.ndarray], np.ndarray]],
     ):
         """Initializes the EMap class.
 
         Args:
             predictor_fn (Callable): A Callable that takes in a dictionary of input np.ndarrays and returns
                 an np.ndarray containing predictions. The predictor_fn should take arguments from the dataset provided.
-            dataset (Dict[str, np.ndarray): A dictionary of numpy array of the dataset to
-                be used for the analysis. Each value is an np.ndarray over different mode inputs as needed by the predictor.
+            dataset (Dict[str, Union[Dict[str, np.ndarray], Tuple[np.ndarray] ,np.ndarray]]): A dictionary of numpy array, or tuples,
+                or dictionaries of the dataset to be used for the analysis. Each value is an np.ndarray over different mode
+                inputs as needed by the predictor.
         """
         self.predictor_fn = predictor_fn
         self.dataset = dataset
+
+    def get_input_lengths(self):
+        """Returns the input lengths for each mode.
+
+        Returns:
+            List[int]: A list of the lengths for each mode.
+        """
+        # First mode, first value:
+        test_value = list(self.dataset.values())[0]
+        # If it is a batched numpy array, length is same as batch size
+        if isinstance(test_value, np.ndarray):
+            input_lengths = list(map(lambda x: len(x), self.dataset.values()))
+        elif isinstance(test_value, Tuple):
+            # Take the length of the first input in that mode
+            input_lengths = list(map(lambda x: len(x[0]), self.dataset.values()))
+        elif isinstance(test_value, Dict):
+            # Take the length of the first key in the dictionary for that mode
+            input_lengths = list(
+                map(
+                    lambda x: len(x[list(x.keys())[0]]),
+                    self.dataset.values(),
+                )
+            )
+        else:
+            raise NotImplementedError(
+                "No implementation found for types other than numpy array, tuple and dict."
+            )
+        return input_lengths
 
     def get_cartesian_product_of_indices(self):
         """Computes the cartesian product of the indices of the dataset.
@@ -35,7 +64,8 @@ class Emap:
             np.ndarray: The cartesian product of index ranges of the dataset.
         """
 
-        index_ranges = list(map(lambda x: list(range(len(x))), self.dataset.values()))
+        input_lengths = self.get_input_lengths()
+        index_ranges = list(map(lambda x: list(range(x)), input_lengths))
         cartesian_product = np.array(list(product(*index_ranges)))
         return cartesian_product
 
@@ -43,23 +73,79 @@ class Emap:
         """Creates a generator to generate inputs with all combinations of different indices.
 
         Args:
-            batch_size[int]: The batch size to use for the generator.
+            batch_size (int): The batch size to use for the generator.
         Returns:
-            Dict[str, List]: A batch of the new dataset which each batch a list of the original input.
+            Dict[str, Union[Dict[str, np.ndarray], Tuple[np.ndarray] ,np.ndarray]]:
+                A batch of the new dataset which each batch a list of the original input.
         """
 
         cartesian_product = self.get_cartesian_product_of_indices()
         keys = list(self.dataset.keys())
 
         for i in range(0, cartesian_product.shape[0], batch_size):
-            output_dict = dict(zip(keys, [[] for i in range(len(keys))]))
+            if isinstance(self.dataset[keys[0]], np.ndarray):
+                output_dict = dict(zip(keys, [[] for i in range(len(keys))]))
+            elif isinstance(self.dataset[keys[0]], Tuple):
+                # num modes = len(keys)
+                # num inputs per mode = len(self.dataset[key])
+                # need one more list for batch size
+                output_dict = dict(
+                    zip(
+                        keys,
+                        [
+                            [[] for k in range(len(self.dataset[key]))]
+                            for j, key in enumerate(keys)
+                        ],
+                    )
+                )
+            # We already check for types in cartesian product method
+            else:
+                output_dict = dict(
+                    zip(
+                        keys,
+                        [
+                            {k: [] for k in self.dataset[key].keys()}
+                            for j, key in enumerate(keys)
+                        ],
+                    )
+                )
+
             batch_index_products = cartesian_product[i : i + batch_size]
             for index_product in batch_index_products:
-                for j, key in enumerate(keys):
-                    output_dict[key].append(self.dataset[key][index_product[j]])
+                for j, mode_key in enumerate(keys):
+                    if isinstance(self.dataset[mode_key], np.ndarray):
+                        output_dict[mode_key].append(
+                            self.dataset[mode_key][index_product[j]]
+                        )
+                    elif isinstance(self.dataset[mode_key], Tuple):
+                        # output_dict[key] will be list of tuples of numpy arrays
+                        # Each numpy array will correspond to one kind of input for the `key` mode.
+                        # We need to convert it to a tuple of numpy arrays with the same length.
+                        # The numpy arrays in each have to be stacked togther.
+                        for tuple_index in range(len(self.dataset[mode_key])):
+                            output_dict[mode_key][tuple_index].append(
+                                self.dataset[mode_key][tuple_index][index_product[j]]
+                            )
+                    else:
+                        for dict_key in self.dataset[mode_key].keys():
+                            output_dict[mode_key][dict_key].append(
+                                self.dataset[mode_key][dict_key][index_product[j]]
+                            )
 
-            for key in keys:
-                output_dict[key] = np.array(output_dict[key])
+            # Convert internal arrays to numpy arrays
+            for mode_key in keys:
+                if isinstance(self.dataset[mode_key], np.ndarray):
+                    output_dict[mode_key] = np.array(output_dict[mode_key])
+                elif isinstance(self.dataset[mode_key], Tuple):
+                    for input_index in range(len(output_dict[mode_key])):
+                        output_dict[mode_key][input_index] = np.array(
+                            output_dict[mode_key][input_index]
+                        )
+                else:
+                    for dict_key in self.dataset[mode_key].keys():
+                        output_dict[mode_key][dict_key] = np.array(
+                            output_dict[mode_key][dict_key]
+                        )
             yield output_dict
 
     def dataset_generator(self, batch_size: int = 32):
@@ -70,12 +156,38 @@ class Emap:
         """
 
         keys = list(self.dataset.keys())
-        for i in range(0, len(self.dataset[keys[0]]), batch_size):
-            yield {key: self.dataset[key][i : i + batch_size] for key in keys}
+        first_value = list(self.dataset.values())[0]
+        if isinstance(first_value, np.ndarray):
+            for i in range(0, len(first_value), batch_size):
+                yield {key: self.dataset[key][i : i + batch_size] for key in keys}
+        elif isinstance(first_value, Tuple):
+            for i in range(0, len(first_value[0]), batch_size):
+                yield {
+                    key: tuple(
+                        [
+                            input_array[i : i + batch_size]
+                            for input_array in self.dataset[key]
+                        ]
+                    )
+                    for key in keys
+                }
+        elif isinstance(first_value, Dict):
+            first_value_value = list(first_value.values())[0]
+            for i in range(0, len(first_value_value), batch_size):
+                yield {
+                    key: {
+                        dict_key: self.dataset[key][dict_key][i : i + batch_size]
+                        for dict_key in self.dataset[key].keys()
+                    }
+                    for key in keys
+                }
 
     def compute_predictions(self, typ: str = "emap", batch_size: int = 32):
         """Computes the predictions on the dataset of given type for a given predictor.
 
+        Args:
+            typ (str): The type of predictions to compute. One of "emap" or "orig".
+            batch_size (int): The batch size to use for the generator.
         Returns:
             np.ndarray: A numpy array of predictions for a given predictor.
 
@@ -106,7 +218,7 @@ class Emap:
 
         logits_mean = np.mean(emap_predictions, axis=0)
 
-        input_lengths = list(map(lambda x: len(x), self.dataset.values()))
+        input_lengths = self.get_input_lengths()
         emap_predictions = emap_predictions.reshape(*input_lengths, -1)
 
         all_axes = list(range(emap_predictions.ndim - 1))
--- tests/mma/analysis/metrics/test_emap.py
+++ tests/mma/analysis/metrics/test_emap.py
@@ -207,3 +207,493 @@ class TestEmap(unittest.TestCase):
                 atol=1e-5,
             )
         )
+
+
+class TestEmapTuple(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+
+        cls.dataset = {
+            "arg0": (
+                np.array(
+                    [
+                        [1.12743584, 1.13448005],
+                        [0.50889023, 0.46084774],
+                        [-0.28415428, -0.07687285],
+                        [0.15706401, 0.26843169],
+                        [-1.70497695, 1.19067221],
+                    ]
+                ),
+                np.array(
+                    [
+                        [-0.75966783, 1.14106388, 0.27610688],
+                        [0.66013527, 0.27800452, -0.37362844],
+                        [0.96804656, 0.21056764, 0.38410439],
+                        [0.52634001, -0.4904973, -0.2707183],
+                        [0.50303166, -1.27711476, 0.45915383],
+                    ]
+                ),
+            ),
+            "arg1": (
+                np.array(
+                    [
+                        [1.13190186, 0.34093785],
+                        [2.42764734, 0.16388448],
+                        [0.7282957, 0.57181146],
+                        [-1.29928076, -0.49100054],
+                        [-0.2341222, 0.40688819],
+                    ]
+                ),
+            ),
+        }
+
+        cls.w0 = np.array([[0.33023155], [1.40766588]])
+        cls.w1 = np.array([[-0.89154714], [-0.78821395], [1.50791622]])
+        cls.b = np.array([[0.17281638]])
+        cls.predictor_fn = lambda arg0, arg1: np.concatenate(
+            [
+                (arg0[0] @ cls.w0) * (arg0[1] @ cls.w1) * (arg1[0] @ cls.w0),
+                (arg0[0] @ cls.w0) + (arg0[1] @ cls.w1) * (arg1[0] @ cls.w0),
+            ],
+            axis=1,
+        )
+
+        cls.batch_size = 4
+
+        cls.emap_obj = Emap(cls.predictor_fn, cls.dataset)
+
+    def test_get_cartesian_product_of_indices(self):
+        cartesian_product = self.emap_obj.get_cartesian_product_of_indices()
+        self.assertEqual(cartesian_product.shape, (25, 2))
+
+        self.assertTrue(
+            (
+                cartesian_product
+                == np.array(
+                    [
+                        [0, 0],
+                        [0, 1],
+                        [0, 2],
+                        [0, 3],
+                        [0, 4],
+                        [1, 0],
+                        [1, 1],
+                        [1, 2],
+                        [1, 3],
+                        [1, 4],
+                        [2, 0],
+                        [2, 1],
+                        [2, 2],
+                        [2, 3],
+                        [2, 4],
+                        [3, 0],
+                        [3, 1],
+                        [3, 2],
+                        [3, 3],
+                        [3, 4],
+                        [4, 0],
+                        [4, 1],
+                        [4, 2],
+                        [4, 3],
+                        [4, 4],
+                    ]
+                )
+            ).all()
+        )
+
+    def test_combination_generator(self):
+        comb_gen = self.emap_obj.combination_generator(self.batch_size)
+        batch = comb_gen.__next__()
+        self.assertEqual(batch.keys(), self.dataset.keys())
+
+        # Integration Test
+        expected_output = {
+            "arg0": (
+                np.array(
+                    [
+                        [1.12743584, 1.13448005],
+                        [1.12743584, 1.13448005],
+                        [1.12743584, 1.13448005],
+                        [1.12743584, 1.13448005],
+                    ]
+                ),
+                np.array(
+                    [
+                        [-0.75966783, 1.14106388, 0.27610688],
+                        [-0.75966783, 1.14106388, 0.27610688],
+                        [-0.75966783, 1.14106388, 0.27610688],
+                        [-0.75966783, 1.14106388, 0.27610688],
+                    ]
+                ),
+            ),
+            "arg1": (
+                np.array(
+                    [
+                        [1.13190186, 0.34093785],
+                        [2.42764734, 0.16388448],
+                        [0.7282957, 0.57181146],
+                        [-1.29928076, -0.49100054],
+                    ]
+                ),
+            ),
+        }
+
+        for key in batch.keys():
+            for index in range(len(batch[key])):
+                self.assertTrue(
+                    np.allclose(
+                        batch[key][index], expected_output[key][index], atol=1e-7
+                    )
+                )
+
+    def test_dataset_generator(self):
+        data_gen = self.emap_obj.dataset_generator(self.batch_size)
+        batch = data_gen.__next__()
+        self.assertEqual(batch.keys(), self.dataset.keys())
+
+        # Integration Test
+        for key in batch.keys():
+            for index in range(len(batch[key])):
+                self.assertTrue(
+                    np.allclose(
+                        batch[key][index],
+                        self.dataset[key][index][: self.batch_size],
+                        atol=1e-7,
+                    )
+                )
+
+    def test_compute_predictions(self):
+        orig_predictions = self.emap_obj.compute_predictions("orig", self.batch_size)
+        self.assertEqual(orig_predictions.shape, (5, 2))
+
+        self.assertTrue(
+            np.allclose(
+                orig_predictions,
+                self.emap_obj.predictor_fn(**self.dataset),
+                atol=1e-5,
+            )
+        )
+
+        emap_predictions = self.emap_obj.compute_predictions("emap", self.batch_size)
+        self.assertEqual(emap_predictions.shape, (25, 2))
+
+        # Integration Test
+        self.assertTrue(
+            np.allclose(
+                orig_predictions,
+                np.array(
+                    [
+                        [0.32653, 2.1350953],
+                        [-1.15611096, -0.59869357],
+                        [0.09501677, -0.67231629],
+                        [0.23629797, 0.97960555],
+                        [0.68960321, 1.73260353],
+                    ]
+                ),
+                atol=1e-5,
+            )
+        )
+
+        self.assertTrue(
+            np.allclose(
+                emap_predictions,
+                np.array(
+                    [
+                        [0.32653, 2.1350953],
+                        [0.39486555, 2.16979601],
+                        [0.39985515, 2.17232973],
+                        [-0.42846567, 1.75170937],
+                        [0.18949933, 2.06551128],
+                        [-0.95603405, -0.3537328],
+                        [-1.15611096, -0.59869357],
+                        [-1.17071982, -0.61657966],
+                        [1.25448739, 2.35268157],
+                        [-0.55482746, 0.13747767],
+                        [0.07759266, -0.58607882],
+                        [0.0938311, -0.66644803],
+                        [0.09501677, -0.67231629],
+                        [-0.10181542, 0.30186902],
+                        [0.04503034, -0.42491752],
+                        [-0.18008065, 0.01067391],
+                        [-0.21776758, -0.07702524],
+                        [-0.22051933, -0.0834287],
+                        [0.23629797, 0.97960555],
+                        [-0.10450851, 0.18653366],
+                        [1.18826877, 2.18062821],
+                        [1.4369473, 2.40405274],
+                        [1.45510486, 2.42036635],
+                        [-1.55922081, -0.2878461],
+                        [0.68960321, 1.73260353],
+                    ]
+                ),
+                atol=1e-5,
+            )
+        )
+
+    def test_compute_emap_scores(self):
+        emap_scores = self.emap_obj.compute_emap_scores(self.batch_size)
+        self.assertEqual(emap_scores.shape, (5, 2))
+
+        # Integration Test
+        self.assertTrue(
+            np.allclose(
+                emap_scores,
+                np.array(
+                    [
+                        [0.21839781, 1.99085081],
+                        [-0.4556023, 0.08521234],
+                        [0.10436421, -0.51085873],
+                        [-0.26637333, 0.47752103],
+                        [0.64578564, 1.68404799],
+                    ]
+                ),
+                atol=1e-5,
+            )
+        )
+
+
+class TestEmapDict(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+
+        cls.dataset = {
+            "arg0": {
+                "arg0_input0": np.array(
+                    [
+                        [1.12743584, 1.13448005],
+                        [0.50889023, 0.46084774],
+                        [-0.28415428, -0.07687285],
+                        [0.15706401, 0.26843169],
+                        [-1.70497695, 1.19067221],
+                    ]
+                ),
+                "arg0_input1": np.array(
+                    [
+                        [-0.75966783, 1.14106388, 0.27610688],
+                        [0.66013527, 0.27800452, -0.37362844],
+                        [0.96804656, 0.21056764, 0.38410439],
+                        [0.52634001, -0.4904973, -0.2707183],
+                        [0.50303166, -1.27711476, 0.45915383],
+                    ]
+                ),
+            },
+            "arg1": {
+                "arg1_input0": np.array(
+                    [
+                        [1.13190186, 0.34093785],
+                        [2.42764734, 0.16388448],
+                        [0.7282957, 0.57181146],
+                        [-1.29928076, -0.49100054],
+                        [-0.2341222, 0.40688819],
+                    ]
+                ),
+            },
+        }
+
+        cls.w0 = np.array([[0.33023155], [1.40766588]])
+        cls.w1 = np.array([[-0.89154714], [-0.78821395], [1.50791622]])
+        cls.b = np.array([[0.17281638]])
+        cls.predictor_fn = lambda arg0, arg1: np.concatenate(
+            [
+                (arg0["arg0_input0"] @ cls.w0)
+                * (arg0["arg0_input1"] @ cls.w1)
+                * (arg1["arg1_input0"] @ cls.w0),
+                (arg0["arg0_input0"] @ cls.w0)
+                + (arg0["arg0_input1"] @ cls.w1) * (arg1["arg1_input0"] @ cls.w0),
+            ],
+            axis=1,
+        )
+
+        cls.batch_size = 4
+
+        cls.emap_obj = Emap(cls.predictor_fn, cls.dataset)
+
+    def test_get_cartesian_product_of_indices(self):
+        cartesian_product = self.emap_obj.get_cartesian_product_of_indices()
+        self.assertEqual(cartesian_product.shape, (25, 2))
+
+        self.assertTrue(
+            (
+                cartesian_product
+                == np.array(
+                    [
+                        [0, 0],
+                        [0, 1],
+                        [0, 2],
+                        [0, 3],
+                        [0, 4],
+                        [1, 0],
+                        [1, 1],
+                        [1, 2],
+                        [1, 3],
+                        [1, 4],
+                        [2, 0],
+                        [2, 1],
+                        [2, 2],
+                        [2, 3],
+                        [2, 4],
+                        [3, 0],
+                        [3, 1],
+                        [3, 2],
+                        [3, 3],
+                        [3, 4],
+                        [4, 0],
+                        [4, 1],
+                        [4, 2],
+                        [4, 3],
+                        [4, 4],
+                    ]
+                )
+            ).all()
+        )
+
+    def test_combination_generator(self):
+        comb_gen = self.emap_obj.combination_generator(self.batch_size)
+        batch = comb_gen.__next__()
+
+        self.assertEqual(batch.keys(), self.dataset.keys())
+
+        # Integration Test
+        expected_output = {
+            "arg0": {
+                "arg0_input0": np.array(
+                    [
+                        [1.12743584, 1.13448005],
+                        [1.12743584, 1.13448005],
+                        [1.12743584, 1.13448005],
+                        [1.12743584, 1.13448005],
+                    ]
+                ),
+                "arg0_input1": np.array(
+                    [
+                        [-0.75966783, 1.14106388, 0.27610688],
+                        [-0.75966783, 1.14106388, 0.27610688],
+                        [-0.75966783, 1.14106388, 0.27610688],
+                        [-0.75966783, 1.14106388, 0.27610688],
+                    ]
+                ),
+            },
+            "arg1": {
+                "arg1_input0": np.array(
+                    [
+                        [1.13190186, 0.34093785],
+                        [2.42764734, 0.16388448],
+                        [0.7282957, 0.57181146],
+                        [-1.29928076, -0.49100054],
+                    ]
+                ),
+            },
+        }
+
+        for key in batch.keys():
+            for dict_key in batch[key].keys():
+                self.assertTrue(
+                    np.allclose(
+                        batch[key][dict_key], expected_output[key][dict_key], atol=1e-7
+                    )
+                )
+
+    def test_dataset_generator(self):
+        data_gen = self.emap_obj.dataset_generator(self.batch_size)
+        batch = data_gen.__next__()
+        self.assertEqual(batch.keys(), self.dataset.keys())
+
+        # Integration Test
+        for key in batch.keys():
+            for dict_key in batch[key].keys():
+                self.assertTrue(
+                    np.allclose(
+                        batch[key][dict_key],
+                        self.dataset[key][dict_key][: self.batch_size],
+                        atol=1e-7,
+                    )
+                )
+
+    def test_compute_predictions(self):
+        orig_predictions = self.emap_obj.compute_predictions("orig", self.batch_size)
+        self.assertEqual(orig_predictions.shape, (5, 2))
+
+        self.assertTrue(
+            np.allclose(
+                orig_predictions,
+                self.emap_obj.predictor_fn(**self.dataset),
+                atol=1e-5,
+            )
+        )
+
+        emap_predictions = self.emap_obj.compute_predictions("emap", self.batch_size)
+        self.assertEqual(emap_predictions.shape, (25, 2))
+
+        # Integration Test
+        self.assertTrue(
+            np.allclose(
+                orig_predictions,
+                np.array(
+                    [
+                        [0.32653, 2.1350953],
+                        [-1.15611096, -0.59869357],
+                        [0.09501677, -0.67231629],
+                        [0.23629797, 0.97960555],
+                        [0.68960321, 1.73260353],
+                    ]
+                ),
+                atol=1e-5,
+            )
+        )
+
+        self.assertTrue(
+            np.allclose(
+                emap_predictions,
+                np.array(
+                    [
+                        [0.32653, 2.1350953],
+                        [0.39486555, 2.16979601],
+                        [0.39985515, 2.17232973],
+                        [-0.42846567, 1.75170937],
+                        [0.18949933, 2.06551128],
+                        [-0.95603405, -0.3537328],
+                        [-1.15611096, -0.59869357],
+                        [-1.17071982, -0.61657966],
+                        [1.25448739, 2.35268157],
+                        [-0.55482746, 0.13747767],
+                        [0.07759266, -0.58607882],
+                        [0.0938311, -0.66644803],
+                        [0.09501677, -0.67231629],
+                        [-0.10181542, 0.30186902],
+                        [0.04503034, -0.42491752],
+                        [-0.18008065, 0.01067391],
+                        [-0.21776758, -0.07702524],
+                        [-0.22051933, -0.0834287],
+                        [0.23629797, 0.97960555],
+                        [-0.10450851, 0.18653366],
+                        [1.18826877, 2.18062821],
+                        [1.4369473, 2.40405274],
+                        [1.45510486, 2.42036635],
+                        [-1.55922081, -0.2878461],
+                        [0.68960321, 1.73260353],
+                    ]
+                ),
+                atol=1e-5,
+            )
+        )
+
+    def test_compute_emap_scores(self):
+        emap_scores = self.emap_obj.compute_emap_scores(self.batch_size)
+        self.assertEqual(emap_scores.shape, (5, 2))
+
+        # Integration Test
+        self.assertTrue(
+            np.allclose(
+                emap_scores,
+                np.array(
+                    [
+                        [0.21839781, 1.99085081],
+                        [-0.4556023, 0.08521234],
+                        [0.10436421, -0.51085873],
+                        [-0.26637333, 0.47752103],
+                        [0.64578564, 1.68404799],
+                    ]
+                ),
+                atol=1e-5,
+            )
+        )
