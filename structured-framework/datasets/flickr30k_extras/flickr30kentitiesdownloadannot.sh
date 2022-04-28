b887d82fe20e3064738b2636d1a0cb3a3688c7ad
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2022-04-29 02:37:20 +0530

    Fix download Flickr30K entities bash script

--- structured-framework/datasets/flickr30k_extras/flickr30kentitiesdownloadannot.sh
+++ structured-framework/datasets/flickr30k_extras/flickr30kentitiesdownloadannot.sh
@@ -4,7 +4,10 @@ mkdir -p data
 mkdir -p data/flickr30k
 cd data/flickr30k
 
-git clone https://github.com/BryanPlummer/flickr30k_entities
+folder="flickr30k_entities"
+if ! git clone "${https://github.com/BryanPlummer/flickr30k_entities}" "${folder}" 2>/dev/null && [ -d "${folder}" ] ; then
+    echo "Clone failed because the folder ${folder} exists"
+fi
 unzip -n flickr30k_entities/annotations.zip
 rm -rf flickr30k_entities
 cd ../..
